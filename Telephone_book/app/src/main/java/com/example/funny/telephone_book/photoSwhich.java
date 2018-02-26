6d7ec5aacd82e354081967b1fc0f0d28fa5fd515
Yaroslav <Funny@localhost.localdomain>
2018-02-26 17:58:18 +0300

    Final version

---
+++ Telephone_book/app/src/main/java/com/example/funny/telephone_book/photoSwhich.java
@@ -0,0 +1,13 @@
+package com.example.funny.telephone_book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class photoSwhich extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photoswhich);
+    }
+}
